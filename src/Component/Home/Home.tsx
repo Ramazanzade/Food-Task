3348ebbcc1a349d33fbb1faf30e3835bf95a2499
Nizam <ramazanov570633@gmail.com>
2023-11-10 02:58:06 +0400

    Home

--- src/Component/Home/Home.tsx
+++ src/Component/Home/Home.tsx
@@ -1,10 +1,32 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Modal, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 import homecss from './homecss'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import { faFilter, faXmark } from '@fortawesome/free-solid-svg-icons';
+import SelectDropdown from 'react-native-select-dropdown'
+
 const Home = () => {
     const [search, setsearch] = useState('')
+    const [modalVisible, setModalVisible] = useState(false);
+    const [price, setprice] = useState('')
+    const food = ["Egypt", "Canada", "Australia", "Ireland"]
+    const [loading, setLoading] = useState(false);
+
+    const openModal = () => {
+        setModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
+    const handleSearch = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+
+    };
+
     return (
         <View style={homecss.view}>
             <View style={homecss.view1}>
@@ -20,9 +42,59 @@ const Home = () => {
                         placeholder='Find you dishes'
                     />
                 </View>
-                <View>
-                    <FontAwesomeIcon icon={faFilter} style={homecss.icon} color='#FF7269' size={20}/>
+                <View style={homecss.iconview}>
+                    <TouchableOpacity onPress={openModal}>
+                        <FontAwesomeIcon icon={faFilter} style={homecss.icon} color='#FF7269' size={20} />
+                    </TouchableOpacity>
                 </View>
+                <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={closeModal}
+                >
+                    <View style={homecss.modalContainer}>
+                        <TouchableOpacity onPress={closeModal}>
+                            <FontAwesomeIcon icon={faXmark} style={homecss.modealicon} color='#FF7269' size={30} />
+                        </TouchableOpacity>
+                        <View>
+                            <View style={homecss.modelview}>
+                                <Text style={homecss.text2}>Price</Text>
+                                <TextInput
+                                    style={homecss.modalinput}
+                                    onChangeText={setprice}
+                                    value={price}
+                                    placeholder='Price'
+                                    keyboardType="numeric"
+                                />
+                            </View>
+                            <View style={homecss.view2}>
+                                <Text style={homecss.text2}> Food Name</Text>
+                                <SelectDropdown
+                                    data={food}
+                                    onSelect={(selectedItem, index) => {
+                                        console.log(selectedItem, index)
+                                    }}
+                                    buttonStyle={{ backgroundColor: '#353842', marginHorizontal: '5%', width: '90%', borderRadius: 10 }}
+                                    buttonTextStyle={{ color: 'gray', marginRight: '50%' }}
+                                    defaultButtonText='Food name'
+                                    rowStyle={{ backgroundColor: '#353842', marginBottom: '0%', borderRadius: 10 }}
+                                    rowTextStyle={{ color: 'gray' }}
+                                    dropdownStyle={{ backgroundColor: '#353842', borderRadius: 10, marginTop: 10 }}
+                                />
+                            </View>
+                            <View style={homecss.toucview}>
+                                <TouchableOpacity style={homecss.touc} onPress={handleSearch}>
+                                    {loading ? (
+                                        <ActivityIndicator size='small' color="white" style={{ marginTop: '5%' }} />
+                                    ) : (
+                                        <Text style={homecss.touctext}>Search</Text>
+                                    )}
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </View>
         </View>
     )
--- src/Component/Home/homecss.tsx
+++ src/Component/Home/homecss.tsx
@@ -17,17 +17,73 @@ export default StyleSheet.create({
     },
     input: {
         backgroundColor: "#353842",
-        width:'90%',
-        marginHorizontal:'5%',
-        fontSize:20,
-        paddingHorizontal:20,
-        padding:'4%',
-        borderRadius:10
+        width: '90%',
+        marginHorizontal: '5%',
+        fontSize: 15,
+        paddingHorizontal: 20,
+        padding: '4%',
+        borderRadius: 10,
+        color: '#FFFFFF'
     },
-    icon:{
+    icon: {
+        margin: '7%',
+        marginTop: '24%'
+    },
+    inputview: {
+        display: 'flex',
+        position: 'relative'
+    },
+    iconview: {
+        position: 'absolute',
+        zIndex: 1,
+        alignSelf: 'flex-end'
 
     },
-    inputview:{
-        
+    modalContainer: {
+        backgroundColor: '#2C2F38',
+        width: '65%',
+        flex: 1
+    },
+    modealicon: {
+        alignSelf: 'flex-end',
+        margin: '5%',
+        marginTop: '10%'
+    },
+    modalinput: {
+        backgroundColor: "#353842",
+        width: '90%',
+        marginHorizontal: '5%',
+        fontSize: 15,
+        paddingHorizontal: 20,
+        padding: '4%',
+        borderRadius: 10,
+        color: 'gray'
+    },
+    text2: {
+        marginHorizontal: '9%',
+        fontSize: 15,
+        color: '#FFFFFF',
+        marginBottom: '5%'
+    },
+    view2: {
+        marginTop: '15%'
+    },
+    modelview: {
+        marginTop: '50%'
+    },
+    toucview: {
+        marginTop: '40%'
+    },
+    touc: {
+        backgroundColor: '#FF7269',
+        width: '60%',
+        borderRadius: 5,
+        height: 40,
+        alignSelf: 'flex-end'
+    },
+    touctext: {
+        fontSize: 15,
+        textAlign: 'center',
+        marginTop: '3%'
     }
 })
